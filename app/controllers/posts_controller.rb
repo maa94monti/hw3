44ce4da206d569e1cd59462d3281af763400f9fb
maa94monti Monti <mario.monti@kellogg.northwestern.edu>
2023-05-17 17:12:36 +0000

    Almost final code synch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
+  
+  def new
+    @post = Post.new
+    @post["place_id"] = params["place_id"]
+  end
+
+  def create
+    @post = Post.new
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post["place_id"] = params["post"]["place_id"]
+        
+    @post.save
+    redirect_to "/places/#{@post["place_id"]}"
 
   end
-end
-
-def new
-  @post = Post.new
 
 end
